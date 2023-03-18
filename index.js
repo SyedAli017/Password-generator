2569a69a7f9a78182aebbba59005352179ecc9e2
SyedAli017 <124472340+SyedAli017@users.noreply.github.com>
2023-03-18 21:52:31 +0530

    updated code for JavaScript

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
- "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[",
- "}","]",",","|",":",";","<",">",".","?","/"];
-
-
-let PasswordOne = document.getElementById("password-one")
-let PasswordTwo = document.getElementById("password-two")
-
-
 function generatePassword() {
-    for (let i = 0; i < 15; i++) {
-       let randomIndexOne = Math.floor( Math.random() * characters.length)
-       let randomIndexTwo = Math.floor( Math.random() * characters.length)
-       PasswordOne.textContent += characters[randomIndexOne]
-       PasswordTwo.textContent += characters[randomIndexTwo]
-    }   
-}
+  const characters =
+    "A/BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()_-+={[}],|:;<>.?/";
 
+  let passwordLength = 15;
+  let passwordOneText = " ";
+  let passwordTwoText = " ";
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomIndexOne = Math.floor(Math.random() * characters.length);
+    let randomIndexTwo = Math.floor(Math.random() * characters.length);
+
+    passwordOneText += characters.substring(randomIndexOne, randomIndexOne + 1);
+    passwordTwoText += characters.substring(randomIndexTwo, randomIndexTwo + 1);
+  }
+
+  let passwordOne = document.getElementById("password-one");
+  let passwordTwo = document.getElementById("password-two");
+
+  passwordOne.textContent = passwordOneText;
+  passwordTwo.textContent = passwordTwoText;
+}
